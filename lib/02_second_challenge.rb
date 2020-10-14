38ae5ccccd2393e6c106ad920cc805001b8db74b
simon cohen <escohen115@gmail.com>
2020-10-14 23:32:14 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,20 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
-      #your key/value pairs here!
-      },
-    :capulet => {
-      :patriarch => {}
-      :matriarch => {}
-      :heroine => {}
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends => []
-      #your key/value pairs here!
-      }
-    }
+   }
+}
 
 
 end
